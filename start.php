68a4e7c7370f324257377fc0fab426957c1ef40a
Jeremi971 <79988469+Jeremi971@users.noreply.github.com>
2022-05-10 17:01:04 +0200

    code start
    code css/html de la page start

---
+++ start.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+    </style>
+</head>
+<body>
+    <div class="background">
+        <div class="logo"></div>
+        <p class="text">PRESS R START</p>
+        <P class="text">EXIT</P>
+
+    </div>
+
+</body>
+</html>
